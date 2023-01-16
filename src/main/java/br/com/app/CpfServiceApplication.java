40f3776ae3313e2efa77fddd69144b3089f43357
Carlos Maciel <carlos.maciel@sbwebservices.net>
2023-01-16 01:42:18 -0300

    initil commit

---
+++ src/main/java/br/com/app/CpfServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpfServiceApplication.class, args);
+	}
+
+}
